508bfcbf9a4f0420553f37c29738ade3e73216d4
Liu Jinlong <nnuljl@gmail.com>
2016-11-22 10:45:15 +0800

    注解@EnableCaching 移入AppConfig内。

--- src/main/java/top/onos/CacheInPractiseApplication.java
+++ src/main/java/top/onos/CacheInPractiseApplication.java
@@ -3,10 +3,8 @@ package top.onos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class CacheInPractiseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
